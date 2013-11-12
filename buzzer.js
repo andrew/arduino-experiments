40ee8f80bbea0e14ce241f2d20dc95a6940c0e13
Andrew Nesbitt <andrewnez@gmail.com>
2013-11-12 21:39:35 +0000

    Added Beeping script

--- buzzer.js
+++ buzzer.js
@@ -1,15 +1,17 @@
-var five = require("johnny-five"),
-    // or "./lib/johnny-five" when running from the source
-    board = new five.Board();
+var five = require("johnny-five");
 
-board.on("ready", function() {
-
-  buzzer = new five.Pin({
-    addr: 6
-  });
-  
-  board.repl.inject({
-    buzzer: buzzer
-  });
-  
+five.Board().on("ready", function() {
+  var val = 0;
+  var piezoPin = 9;
+  // Set pin 9 to PWM mode
+  this.pinMode( piezoPin, 3 );
+  // beep continously
+  this.loop(10, function(){
+      if (val){
+          this.analogWrite( piezoPin, 20 );
+      } else {
+          this.analogWrite(piezoPin, 0);
+      }
+      val = val ? 0 : 1;
+  })
 });
